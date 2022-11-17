8bda65b67269399242d09c578ccf8c8ccd00bf22
andrey <andreiluka@ya.ru>
2022-11-17 15:11:44 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="container">
+    <div class="header">
+
+    </div>
+    <page-cart msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import pageCart from './components/pageCart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    pageCart
   }
 }
 </script>
 
 <style lang="scss">
+// @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Roboto:wght@500;700&display=swap');
+@import "normalize.css";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: 'Roboto', sans-serif;
+  font-family: 'Lato', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.header {
+  // height: 95px;
+  padding: 23px 0;
 }
 </style>
---
+++ src/components/pageCart.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="hello">
+      <h5>pageCart test text</h5>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'pageCart',
+   props: {
+     msg: String
+   }
+ }
+ </script>
+ 
+ <style scoped lang="scss">
+ .hello {
+   font-size: 30px;
+   margin: 40px 0 0;
+ }
+ ul {
+   list-style-type: none;
+   padding: 0;
+ }
+ li {
+   display: inline-block;
+   margin: 0 10px;
+ }
+ a {
+   color: #42b983;
+ }
+ </style>
